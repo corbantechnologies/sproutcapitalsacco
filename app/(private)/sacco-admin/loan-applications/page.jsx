6322908546988039e74c01091dd21d682a21501f
Dalienst <oduordalienst@gmail.com>
2026-07-09 21:05:23 +0300

    update applications

--- app/(private)/sacco-admin/loan-applications/page.jsx
+++ app/(private)/sacco-admin/loan-applications/page.jsx
@@ -229,8 +229,8 @@ export default function AdminLoanApplications() {
           </Popover>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <div className="lg:col-span-2 flex flex-col gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+          <div className="lg:col-span-3 flex flex-col gap-4">
 
 
             {/* Controls Section */}
