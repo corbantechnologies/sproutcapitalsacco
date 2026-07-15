39d6c5b97bb705a19790c728912fa6bf44346011
Dalienst <oduordalienst@gmail.com>
2026-07-15 09:47:41 +0300

    update width

--- app/(private)/sacco-admin/loan-applications/[reference]/page.jsx
+++ app/(private)/sacco-admin/loan-applications/[reference]/page.jsx
@@ -525,9 +525,9 @@ const LoanApplicationDetailSkeleton = () => (
           </div>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
           {/* Main Content - Left Column */}
-          <div className="lg:col-span-2 space-y-6">
+          <div className="lg:col-span-3 space-y-6">
             {/* Financial Overview */}
             <Card>
               <CardHeader>
